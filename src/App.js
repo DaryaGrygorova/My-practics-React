523ce6c9f8419cc388f7cf95be05cd3145dab500
DaryaGrygorova <grygorova.darya@gmail.com>
2022-04-22 23:38:56 +0300

    add new component and return it in App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h>Новая фишка</h>
+      <Header />
+     <h>Новая фишка</h>
      <ul className="list">
        <li className="list__item list__item--color">Изменение цвета</li>
        <li className="list__item list__item--inline">Строчный єлемент</li>
        <li className="list__item list__item--big-size">Изменение шрифта</li>
+       </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="Header">
+     <ul className="nav-list list">
+       <li className="nav-list__item">
+         <a href="" className='nav-list__link'>Home</a></li>
+       <li className="nav-list__item">
+         <a href="" className='nav-list__link'>Library</a></li>
+       <li className="nav-list__item">
+         <a href="" className='nav-list__link'>Contacts</a></li>
        </ul>
     </div>
   );
