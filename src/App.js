c9c9f6ce517717d1ec7d50490932aaeeb3072769
DaryaGrygorova <grygorova.darya@gmail.com>
2022-04-22 23:58:04 +0300

    add component Fitches

--- src/App.js
+++ src/App.js
@@ -5,29 +5,46 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-     <h>Новая фишка</h>
-     <ul className="list">
-       <li className="list__item list__item--color">Изменение цвета</li>
-       <li className="list__item list__item--inline">Строчный єлемент</li>
-       <li className="list__item list__item--big-size">Изменение шрифта</li>
-       </ul>
+      <Fitches />
     </div>
   );
-}
+};
 
 const Header = () => {
   return (
     <div className="Header">
-     <ul className="nav-list list">
-       <li className="nav-list__item">
-         <a href="" className='nav-list__link'>Home</a></li>
-       <li className="nav-list__item">
-         <a href="" className='nav-list__link'>Library</a></li>
-       <li className="nav-list__item">
-         <a href="" className='nav-list__link'>Contacts</a></li>
-       </ul>
+      <ul className="nav-list list">
+        <li className="nav-list__item">
+          <a href="" className="nav-list__link">
+            Home
+          </a>
+        </li>
+        <li className="nav-list__item">
+          <a href="" className="nav-list__link">
+            Library
+          </a>
+        </li>
+        <li className="nav-list__item">
+          <a href="" className="nav-list__link">
+            Contacts
+          </a>
+        </li>
+      </ul>
     </div>
   );
-}
+};
+
+const Fitches = () => {
+  return (
+    <div>
+      <h>Новая фишка</h>
+      <ul className="list">
+        <li className="list__item list__item--color">Изменение цвета</li>
+        <li className="list__item list__item--inline">Строчный єлемент</li>
+        <li className="list__item list__item--big-size">Изменение шрифта</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
