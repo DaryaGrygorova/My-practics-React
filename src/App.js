30156fb85aca0a0942a2b8eb0c1763ac6d1ec485
DaryaGrygorova <grygorova.darya@gmail.com>
2022-04-23 16:11:40 +0300

    add socisls frame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src=".././public/logo192.png" className="logo" alt="logo" with="40" height="40" />
       </header>
+      <aside className="sidebar">
+          <nav className="nav">
+          <ul className="nav__list list">
+            <li className="nav__item">
+              <a className="nav__link" href="" target="_blank" rel="noopener noreferrer">
+                Learn html/css
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="" target="_blank" rel="noopener noreferrer">
+                Learn js
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="" target="_blank" rel="noopener noreferrer">
+                Learn React
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <main className='content'>
+      </main>
+      <footer className='footer'>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
