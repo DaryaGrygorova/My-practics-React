08dcdf43e9165d18b6a822e28dd7fbff6e8fb527
DaryaGrygorova <grygorova.darya@gmail.com>
2022-04-27 23:01:47 +0300

    add massive.map function for Dialogs and Chat

--- src/components/dialogs/chat.jsx
+++ src/components/dialogs/chat.jsx
@@ -1,18 +1,20 @@
 import styles from "./chat.module.css"
 import Message from './message';
+import Contact from './contact';
 
 let MessData = [
-  {message: 'Hello!',  name: '{props.name}', time: '15:20'},
-  {message: 'It\'s my first private chat',  name: '{props.name}', time: '15:21'},
-  {message: 'It\'s my first message!',  name: '{props.name}', time: '15:21'},
-  {message: 'It\'s my first message!',  name: '{props.name}', time: '15:22'},
-  {message: 'It\'s my first message!',  name: '{props.name}', time: '15:22'},
-  {message: 'It\'s my first message!',  name: '{props.name}', time: '15:22'},
-  {message: 'It\'s my first message!',  name: '{props.name}', time: '15:22'},
-  {message: 'It\'s my first message!',  name: '{props.name}', time: '15:22'},
-  {message: 'HaHaHa!!! I like it!!',  name: '{props.name}', time: '15:24'},
+  {id: 1, message: 'Hello!',  name: 'Frank Sinatra', time: '15:20'},
+  {id: 2, message: 'It\'s my first private chat',  name: 'Frank Sinatra', time: '15:21'},
+  {id: 3, message: 'It\'s my first message!',  name: 'Frank Sinatra', time: '15:21'},
+  {id: 4, message: 'It\'s my first message!',  name: 'Frank Sinatra', time: '15:22'},
+  {id: 5, message: 'It\'s my first message!',  name: 'Frank Sinatra', time: '15:22'},
+  {id: 6, message: 'It\'s my first message!',  name: 'Frank Sinatra', time: '15:22'},
+  {id: 7, message: 'It\'s my first message!',  name: 'Frank Sinatra', time: '15:22'},
+  {id: 8, message: 'It\'s my first message!',  name: 'Frank Sinatra', time: '15:22'},
+  {id: 9, message: 'HaHaHa!!! I like it!!',  name: 'Frank Sinatra', time: '15:24'},
 ]
 
+let MessElements = MessData.map ( Mess => <Message id={Mess.id} message={Mess.message} name={Mess.name} time={Mess.time}/> );
 
 
 const Chat = (props) => {
@@ -29,15 +31,7 @@ const Chat = (props) => {
     <h2 className={styles.name}>{props.name}</h2>
   </div>
   <div className={styles.wrapper}>
-    <Message message={MessData[0].message}  name={props.name} time={MessData[0].time} />
-    <Message message={MessData[1].message} name={props.name} time={MessData[1].time} />
-    <Message message={MessData[2].message} name={props.name} time={MessData[2].time} />
-    <Message message={MessData[3].message} name={props.name} time={MessData[3].time} />
-    <Message message={MessData[4].message} name={props.name} time={MessData[4].time} />
-    <Message message={MessData[5].message} name={props.name} time={MessData[5].time} />
-    <Message message={MessData[6].message} name={props.name} time={MessData[6].time} />
-    <Message message={MessData[7].message} name={props.name} time={MessData[7].time} />
-    <Message message={MessData[8].message} name={props.name} time={MessData[8].time} />
+    { MessElements }
   </div>
       <form className={styles.thumb}>
             <textarea
--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -1,8 +1,9 @@
 import Contact from './contact';
 import styles from "./dialogs.module.css"
 import Chat from './chat';
+import Post from '../profile/myPosts/post';
 
-let DialogsData = [
+let ContactsData = [
   { name: 'Sara Connor', id: 'Connor'},
   { name: 'Lisa Fahrenheit', id: 'Fahrenheit'},
   { name: 'Rose Friedman', id: 'Friedman'},
@@ -12,6 +13,8 @@ let DialogsData = [
   { name: 'Lili Walker', id: 'Walker'},
 ];
 
+let ContactsElements = ContactsData.map ( OneContact => <Contact id={OneContact.id} name={OneContact.name} /> );
+
 
 const Dialogs = (props) => {
   return (
@@ -19,16 +22,10 @@ const Dialogs = (props) => {
       <main className={styles.container}>
         <div className={styles.contacts}>
           <h1 className={styles.title}>Dialogs</h1>
-          <Contact name={DialogsData[0].name} id={DialogsData[0].id} />
-          <Contact name={DialogsData[1].name} id={DialogsData[1].id} />
-          <Contact name={DialogsData[2].name} id={DialogsData[2].id} />
-          <Contact name={DialogsData[3].name} id={DialogsData[3].id} />
-          <Contact name={DialogsData[4].name} id={DialogsData[4].id} />
-          <Contact name={DialogsData[5].name} id={DialogsData[5].id} />
-          <Contact name={DialogsData[6].name} id={DialogsData[6].id} />
+          { ContactsElements }
         </div>
         <span className={styles.line}></span>
-        <Chat name={DialogsData[3].name}/>
+        <Chat name={ContactsData[3].name}/>
       </main>
     </div>
   );
