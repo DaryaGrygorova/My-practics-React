4ae3a92ad63cfa09953ec78f7d8b4647be6ebdeb
DaryaGrygorova <grygorova.darya@gmail.com>
2022-05-03 02:43:51 +0300

    chenged if-else to switch-case

--- src/Redux/Reducers/dialogs-reducer.js
+++ src/Redux/Reducers/dialogs-reducer.js
@@ -2,16 +2,21 @@ const ADD_MESS = 'ADD-MESS';
 const UPDATE_MESS_TEXT = 'UPDATE-MESS-TEXT';
 
 export const dialogsReducer = (state, action) => {
- if (action.type === UPDATE_MESS_TEXT) {
-   state.newMessText = action.newText;
+  switch (action.type) {
+    case UPDATE_MESS_TEXT:
+      state.newMessText = action.newText;
+      return state;
+
+    case ADD_MESS:
+      let newMess = { id: 10,
+         name: 'Frank Sinatra',
+        message: (state.newMessText),
+         time: '15:58' };
+       state.MessData.push(newMess);
+       state.newMessText = '';
+      return state;
+
+    default:
+      return state;
   }
- else if (action.type === ADD_MESS) {
-   let newMess = { id: 10,
-     name: 'Frank Sinatra',
-     message: (state.newMessText),
-     time: '15:58' };
-   state.MessData.push(newMess);
-   state.newMessText = '';
- }
-  return state;
-}
+};
--- src/Redux/Reducers/profile-reducer.js
+++ src/Redux/Reducers/profile-reducer.js
@@ -2,18 +2,22 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_POST_TEXT = 'UPDATE-POST-TEXT';
 
 export const profileReducer = (state, action) => {
-  if (action.type === UPDATE_POST_TEXT)  {
-   state.NewPostText = action.newText;
-  }
-  else if (action.type === ADD_POST) {
-    let newPost = { id: 5,
-      name: 'Jolly Docker',
-      message: (state.NewPostText),
-      time: '14:58',
-      likeCounter: 0 };
-    state.PostsData.push(newPost);
-    state.NewPostText = '';
-  }
+  switch (action.type) {
+    case UPDATE_POST_TEXT:
+      state.NewPostText = action.newText;
+      return (state);
 
-  return state;
-}
+    case ADD_POST:
+      let newPost = { id: 5,
+        name: 'Jolly Docker',
+        message: (state.NewPostText),
+        time: '14:58',
+        likeCounter: 0 };
+      state.PostsData.push(newPost);
+      state.NewPostText = '';
+      return (state);
+
+    default:
+      return (state);
+  }
+};
