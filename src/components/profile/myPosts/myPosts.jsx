665da1b5b3dd1036905b55130c8aab0e8ba060a1
DaryaGrygorova <grygorova.darya@gmail.com>
2022-05-03 05:38:20 +0300

    refactoring structure for myPosts

--- src/components/profile/myPosts/myPosts.jsx
+++ src/components/profile/myPosts/myPosts.jsx
@@ -1,9 +1,45 @@
 import Post from './post/post';
+import styles from '../myPosts/myPost.module.css';
+import { addNewPostActionCreator, addPostActionCreator } from '../../../Redux/Reducers/profile-reducer';
 
 const MyPosts = (props) => {
-  return (
+
+let handleChange = (event) => {
+  let newText = event.target.value;
+  props.dispatch(addNewPostActionCreator(newText));
+};
+
+let addPost = () => {
+  props.dispatch(addPostActionCreator());
+}
+
+let Posts = props.PostsData.map ( PostItem => <Post id={PostItem.id} name={PostItem.name} message={PostItem.message} likeCounter={PostItem.likeCounter} time={PostItem.time}/> );
+
+return (
     <div>
-      <Post id={props.id} name={props.name} message={props.message} likeCounter={props.likeCounter} time={props.time}/>
+      <h2 className={styles.title}>My posts</h2>
+      <form className={styles.thumb}>
+        <textarea
+          className={styles.input}
+          name="new_post"
+          rows="3"
+          placeholder="Enter the text"
+          required
+          value = {props.newPostText}
+          onChange={handleChange}
+        />
+        <button className={`${styles.button} button`}
+          // type="submit"
+                onClick={addPost}>
+          Send
+        </button>
+      </form>
+
+
+
+      {/*<NewPost newPostText={props.NewPostText}*/}
+      {/*         dispatch={props.dispatch}/>*/}
+      {Posts}
     </div>
   );
 };
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,18 +1,13 @@
 import MyPosts from "./myPosts/myPosts";
 import User from "./userInfo/userInfo";
-import NewPost from './myPosts/newPost/newPost';
 import styles from './profile.module.css';
 
 const Profile = (props) => {
-  let MyPostsElements = props.States.PostsData.map ( PostItem => <MyPosts id={PostItem.id} name={PostItem.name} message={PostItem.message} likeCounter={PostItem.likeCounter} time={PostItem.time}/> );
   return (
     <main className="content">
       <User name={props.States.Users[0].userName} about={props.States.Users[0].about}/>
       <div className={styles.wrapper}>
-        <h2 className={styles.title}>My posts</h2>
-        <NewPost newPostText={props.States.NewPostText}
-                 dispatch={props.dispatch}/>
-        { MyPostsElements }
+      <MyPosts dispatch={props.dispatch} PostsData={props.States.PostsData} NewPostText={props.States.NewPostText} />
       </div>
     </main>
   );
