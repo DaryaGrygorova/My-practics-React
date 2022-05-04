e7e32891449ad9ebd7df070a22637a6312125e13
DaryaGrygorova <grygorova.darya@gmail.com>
2022-05-04 21:59:35 +0300

    added get.request fo Users to server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,11 @@
 import styles from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../images/customer_person_people_man.png'
 
 let Users = (props) => {
-  // if (props.Users.length === 0) {
-  //  props.setUsers([
-  //     { id: 1, followed: true, userName: 'Jolly Docker', about: 'Pretty lady', photoURL: 'https://cdn.icon-icons.com/icons2/11/PNG/256/customer_person_people_woman_you_1627.png' },
-  //     { id: 2, followed: false, userName: 'Sara Connor', about: 'Astronaut', photoURL: 'https://cdn.icon-icons.com/icons2/11/PNG/256/customer_person_people_woman_you_1627.png ' },
-  //     { id: 3, followed: false, userName: 'Lisa Fahrenheit', about: 'Candy - it\'s my love', photoURL: 'https://cdn.icon-icons.com/icons2/11/PNG/256/customer_person_people_woman_you_1627.png ' },
-  //     { id: 4, followed: true, userName: 'Rose Friedman', about: 'Girl with funny nose', photoURL: 'https://cdn.icon-icons.com/icons2/11/PNG/256/customer_person_people_woman_you_1627.png' },
-  //     { id: 5, followed: false, userName: 'Frank Sinatra', about: 'Music - it\'s all...', photoURL: 'https://ru.seaicons.com/wp-content/uploads/2016/04/user-icon.png ' },
-  //     { id: 6, followed: false, userName: 'John Silver', about: 'Freelancer', photoURL: 'https://ru.seaicons.com/wp-content/uploads/2016/04/user-icon.png ' },
-  //     { id: 7, followed: true, userName: 'Tomas Fisher', about: 'ChokoBanny', photoURL: 'https://ru.seaicons.com/wp-content/uploads/2016/04/user-icon.png ' },
-  //     { id: 8, followed: true, userName: 'Lili Walker', about: 'My little world', photoURL: 'https://cdn.icon-icons.com/icons2/11/PNG/256/customer_person_people_woman_you_1627.png ' }
-  //   ])}
-
-  axios.get("https://social-network.samuraijs.com/api/1.0/users")
-    .then (response => {
-      debugger;
-    props.setUsers()}
-  );
+  if (props.Users.length === 0) {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+         .then (response => { props.setUsers(response.data.items)} )};
 
   return (
     <div className={styles.container}>
@@ -26,13 +14,13 @@ let Users = (props) => {
           <div className={styles.description}>
             <img
               className={styles.photo}
-              src={Card.photoURL}
+              src={Card.photos.small != null ? Card.photos.small : userPhoto}
               alt="User"
               width="40"
               height="40"
             />
-            <h2 className={styles.name}>{Card.userName}</h2>
-            <p className={styles.text}>{Card.about}</p>
+            <h2 className={styles.name}>{Card.name}</h2>
+            <p className={styles.text}>{Card.status}</p>
           </div>
           <div className={styles.buttonSet}>
             {Card.followed
@@ -47,5 +35,6 @@ let Users = (props) => {
       </button>
     </div>
   )
+
 }
 export default Users;
