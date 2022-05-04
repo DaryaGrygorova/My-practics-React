59a4940e9011a44573b1b3759bcb4158796f7c2f
DaryaGrygorova <grygorova.darya@gmail.com>
2022-05-05 00:31:41 +0300

    added first class component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,47 @@
+import React  from 'react';
 import styles from './Users.module.css';
 import axios from 'axios';
 import userPhoto from '../images/customer_person_people_man.png'
 
-let Users = (props) => {
+class Users extends React.Component {
 
-  let gettingUsers = () => {
-    if (props.Users.length === 0) {
+  gettingUsers = () => {
+    if (this.props.Users.length === 0) {
       axios.get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
-          props.setUsers(response.data.items)
+          this.props.setUsers(response.data.items)
         })
-    };
+    }
   }
-  return (
-    <div className={styles.container}>
-      <button className="button" onClick={gettingUsers}>Get Users</button>
-      { props.Users.map ( Card => <div key={Card.id}>
-        <div className={styles.wrapper} >
-          <div className={styles.description}>
-            <img
-              className={styles.photo}
-              src={Card.photos.small != null ? Card.photos.small : userPhoto}
-              alt="User"
-              width="40"
-              height="40"
-            />
-            <h2 className={styles.name}>{Card.name}</h2>
-            <p className={styles.text}>{Card.status}</p>
-          </div>
-          <div className={styles.buttonSet}>
-            {Card.followed
-              ? <button className="button" onClick={() => { props.unfollows(Card.id) } }>Unfollow</button>
-              : <button className="button" onClick={() => { props.follows(Card.id) } }>Follow</button> }
-          </div>
-        </div>
-      </div>)
-      }
-      <button className="button" >
-        Show more
-      </button>
-    </div>
-  )
 
+  render() {
+   return ( <div className={styles.container}>
+    <button className="button" onClick={this.gettingUsers}>Get Users</button>
+    { this.props.Users.map ( Card => <div key={Card.id}>
+      <div className={styles.wrapper} >
+        <div className={styles.description}>
+          <img
+            className={styles.photo}
+            src={Card.photos.small != null ? Card.photos.small : userPhoto}
+            alt="User"
+            width="40"
+            height="40"
+          />
+          <h2 className={styles.name}>{Card.name}</h2>
+          <p className={styles.text}>{Card.status}</p>
+        </div>
+        <div className={styles.buttonSet}>
+          {Card.followed
+            ? <button className="button" onClick={() => { this.props.unfollows(Card.id) } }>Unfollow</button>
+            : <button className="button" onClick={() => { this.props.follows(Card.id) } }>Follow</button> }
+        </div>
+      </div>
+    </div>)
+    }
+    <button className="button" >Show more</button>
+  </div>
+   )
+  }
 }
+
 export default Users;
